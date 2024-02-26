4ea61e6a2df9589783a2403f59d16261b701af16
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2024-02-26 18:20:22 +0000

    Added page base + navbar + incomplete hero section

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Simple page built in HTML, Tailwind and Javascript
+
+*Note: Run the following command when developing in order to build the output.css in real time.*
+`npx tailwindcss -i ./css/input.css -o ./css/output.css --watch`
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./*.{html,js}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				inter: ["Inter", "ui-sans-serif", "system-ui"],
+				goldman: ["Goldman", "ui-sans-serif", "system-ui"],
+			},
+			colors: {
+				accent: "#001E50",
+				foreground: "#1A1A1A",
+			},
+			backgroundImage: {
+				heroBg: "url('../images/hero_image.jpg')",
+			},
+		},
+	},
+	plugins: [],
+};
